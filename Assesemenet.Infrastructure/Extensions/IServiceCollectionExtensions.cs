52ce5793d677a8b2e3dd111c5c6b4afd390c23e7
AnnabiGihed <annabi.gihed@gmail.com>
2023-09-02 08:24:33 +0100

    Infrastructure Project Creation and Implementation

---
+++ Assesemenet.Infrastructure/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Assesement.Domain.Common;
+using Assesement.Application.Interfaces;
+using Assesement.Domain.Common.Interfaces;
+using Assesemenet.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Assesemenet.Infrastructure.Extensions
+{
+	public static class IServiceCollectionExtensions
+	{
+		public static void AddInfrastructureLayer(this IServiceCollection services)
+		{
+			services.AddServices();
+		}
+
+		private static void AddServices(this IServiceCollection services)
+		{
+			services
+				.AddTransient<IMediator, Mediator>()
+				.AddTransient<IDomainEventDispatcher, DomainEventDispatcher>()
+				.AddTransient<IDateTimeService, DateTimeService>()
+		}
+	}
+}
---
+++ Assesemenet.Infrastructure/Services/DataTimeService.cs
@@ -0,0 +1,9 @@
+﻿using Assesement.Application.Interfaces;
+
+namespace Assesemenet.Infrastructure.Services
+{
+	public class DateTimeService : IDateTimeService
+	{
+		public DateTime NowUtc => DateTime.UtcNow;
+	}
+}
